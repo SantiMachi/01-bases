c6df2ca485f85fee21272618d9f4ee1de3b74828
Santiago Machicado <santiago.carlos.machicado@gmail.com>
2023-05-28 23:46:16 -0400

    mi primer proyecto angular
    mi primer proyecto angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  lista:string[]=["20","30","40","50","60","70","70","70","70","70","70","70","70","70",];
   title = 'bases';
+  comprar(valor:string){
+    if(confirm('Costo: '+ valor )){
+      confirm('compra terminada')
+    }
+  }
 }
